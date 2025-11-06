a2ae6a8d62243f272f22ad9b41eacfa095ace3c4
andreiagbisit <alidrey912@gmail.com>
2025-11-06 23:07:56 +0800

    booking confirmation email added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,7 @@ import { Inngest } from 'inngest'
 import User from '../models/User.js'
 import Booking from '../models/Booking.js'
 import Show from '../models/Show.js'
+import sendEmail from '../configs/nodeMailer.js'
 
 // INNGEST - CLIENT TO SEND/RECEIVE EVENTS
 export const inngest = new Inngest({ id: 'movie-ticket-booking' })
@@ -83,9 +84,62 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction (
     }
 )
 
+// INNGEST - SEND EMAIL WHEN USER BOOKS A SHOW
+const sendBookingConfirmationEmail = inngest.createFunction (
+    {id: 'send-booking-confirmation-email'},
+    {event: 'app/show.booked'},
+    async ({ event, step }) => {
+        const { bookingId } = event.data
+        const booking = await Booking.findById(bookingId).populate({
+            path: 'show',
+            populate: {path: 'movie', model: 'Movie'}
+        }).populate('user')
+
+        await sendEmail({
+            to: booking.user.email,
+            subject: `Booking Confirmation: '${booking.show.movie.title}' 🎞️`,
+            body: `
+            <div style="font-family: Arial, sans-serif; background-color: #09090B; padding: 30px;">
+                <div style="max-width: 600px; margin: auto; background: #1A1A1D; padding: 25px; border-radius: 10px; color: #FFFFFF;">       
+                    <h2 style="color: #FFFFFF; margin-top: 0;">
+                        Hello ${booking.user.name},
+                    </h2>
+
+                    <p style="font-size: 15px; line-height: 1.6;">
+                        Ready for <strong style="color: #7545F8;">"${booking.show.movie.title}"</strong>? Your booking is confirmed!
+                    </p>
+
+                    <div style="background: #27272A; padding: 15px; border-radius: 8px; margin: 18px 0;">
+                        <p style="margin: 0; font-size: 15px;">
+                            <strong style="color: #A78BFA;">Date:</strong> 
+                            ${new Date(booking.show.showDateTime).toLocaleDateString('en-PH', { timeZone: 'Asia/Manila' })}
+                            <br/>
+                            
+                            <strong style="color: #A78BFA;">Time:</strong> 
+                            ${new Date(booking.show.showDateTime).toLocaleTimeString('en-PH', { timeZone: 'Asia/Manila' })}
+                        </p>
+                    </div>
+
+                    <p style="font-size: 15px; line-height: 1.6;">
+                        Wishing you a great movie experience! 🍿✨
+                    </p>
+
+                    <p style="font-size: 15px; line-height: 1.6;">
+                        Thanks for choosing <strong style="color: #7545F8;">Kinopia</strong>—see you at the cinema!
+                        <br/><br/>
+                        
+                        <span style="color: #BBBBBB;">— The Kinopia Crew</span>
+                    </p>
+                </div>
+            </div>`
+        })
+    }
+)
+
 export const functions = [
     syncUserCreation,
     syncUserDeletion,
     syncUserUpdation,
-    releaseSeatsAndDeleteBooking
+    releaseSeatsAndDeleteBooking,
+    sendBookingConfirmationEmail
 ]
